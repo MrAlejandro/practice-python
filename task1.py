ac485346b686109ca4377659c8b7220a464f133d
Alexander Shcherbachenko <ascherbachenko@simtechdev.com>
2017-06-03 10:15:52 +0300

    first simple tasks solved

---
+++ task1.py
@@ -0,0 +1,7 @@
+def celcius_to_fahrenheit(celcius = 0):
+    return float(celcius)*1.8 + 32
+
+print(celcius_to_fahrenheit(44.5))
+
+money={"saving_account":200, "checking_account":100, "under_bed":[500,10,100]}
+print(money['under_bed'][1]);
