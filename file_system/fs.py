d006e15d08bd0dc521849caad8ebd6eab9efdd31
Alexander Shcherbachenko <ascherbachenko@simtechdev.com>
2017-06-05 21:54:05 +0300

    more fs examples added + task renamed and modified

--- file_system/fs.py
+++ file_system/fs.py
@@ -1,16 +1,45 @@
 import os
 
 dir_name = os.path.dirname(os.path.abspath(__file__))
-handler = open(dir_name + '/example.txt', 'r')
+file_name = dir_name + '/example.txt'
 
-# fetch all the content from the file
-content = handler.read()
-print(content)
+if (os.path.isfile(file_name)):
+    handler = open(file_name, 'r')
 
-handler.seek(0)
+    # fetch all the content from the file
+    content = handler.read()
+    print(content)
 
-lines = handler.readlines()
-lines = [i.rstrip('\n') for i in lines]
-print(lines)
+    handler.seek(0)
+
+    lines = handler.readlines()
+    lines = [i.rstrip('\n') for i in lines]
+    print(lines)
+
+    handler.close()
+
+    os.remove(file_name)
+
+# the "w" - write mode will remove all the contents from a target file
+handler = open(file_name, 'w')
+
+for i in range(1, 7):
+    handler.write('Line ' + str(i) + '\n')
 
 handler.close()
+
+# use the "a" - append mode to add more content to a file
+handler = open(file_name, 'a')
+
+for i in range(7, 11):
+    handler.write('Apended line ' + str(i) + '\n')
+
+handler.close()
+
+with open(file_name, 'a+') as handler:
+    handler.seek(0)
+    first_line = handler.readline()
+    handler.seek(os.SEEK_END)
+    handler.write(first_line.rstrip('\n') + ' some new content\n')
+    handler.close()
+
--- task.py
+++ task.py
@@ -4,14 +4,21 @@ def celcius_to_fahrenheit(celcius = 0):
     if celcius < -273.15:
         result = 'The temeperature you have specified is too low to be true'
     else:
-        result = round(celcius*1.8 + 32, 2)
+        with open('temps.txt', 'a+') as handler:
+            handler.write(str(round(celcius*1.8 + 32, 2)) + '\n')
+            handler.close()
 
-    return result
+    # return result
 
 temperatures = [10,-20,-289,100]
 
+# empty temps file
+with open('temps.txt', 'w') as handler:
+    handler.close()
+
 for item in temperatures:
-    print(celcius_to_fahrenheit(item))
+    celcius_to_fahrenheit(item)
+    # print(celcius_to_fahrenheit(item))
 
 # money={"saving_account":200, "checking_account":100, "under_bed":[500,10,100]}
 # print(money['under_bed'][1]);
