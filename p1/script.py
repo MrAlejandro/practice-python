be8f10da71aef3b360690a20922775cbdf7f7a25
Alexander Shcherbachenko <ascherbachenko@simtechdev.com>
2017-06-07 21:48:00 +0300

    simple 3 letters word generator implemented

--- p1/script.py
+++ p1/script.py
@@ -3,4 +3,36 @@ import random, string
 def get_random_character(seq):
     return random.choice(str(seq))
 
-print(get_random_character('asfhfalkhdsafhqprnvljxch'))
+lowercase_letters = string.ascii_lowercase
+vowels_list = ['e', 'y', 'u', 'i', 'o', 'a']
+vowels = ''.join(vowels_list)
+consonants = ''.join([l for l in lowercase_letters if l not in vowels_list])
+
+user_input = []
+allowed_input = ('c', 'v', 'a')
+
+for i in range(3):
+    _input = None
+
+    while (not _input):
+        _input = input("Enter 'v' for vowel, 'c' for consonant, and 'a' for any letter: ")
+
+        if (_input in allowed_input):
+            user_input.append(_input)
+        else:
+            _input = None
+            print('Invalid entry, please repeat again!')
+
+result = ''
+
+for char_type in user_input:
+    if (char_type == 'c'):
+        result += get_random_character(consonants)
+    elif (char_type == 'v'):
+        result += get_random_character(vowels)
+    else:
+        result += get_random_character(lowercase_letters)
+
+print(result)
+
+# print(get_random_character('asfhfalkhdsafhqprnvljxch'))
