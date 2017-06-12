9e400b536bc38d5b0d47425ee70b7eed0446605b
Alexander Shcherbachenko <ascherbachenko@simtechdev.com>
2017-06-12 22:15:09 +0300

    Strategy problem2 added

--- opencv/capture.py
+++ opencv/capture.py
@@ -1,4 +1,21 @@
 import cv2
+import matplotlib.pyplot as plt
 
-video = cv2.VideoCapture()
+video = cv2.VideoCapture(0)
+
+a = 0
+
+while True:
+    a += 1
+    check, frame = video.read()
+    plt.imshow(frame, cmap='gray')
+    plt.pause(0.01)
+    plt.draw()
+    key = plt.waitforbuttonpress(0.001)
+
+    if key:
+        break;
+
+print(a)
 video.release()
+cv2.destroyAllWindows()
