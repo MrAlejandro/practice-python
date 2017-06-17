5737f8cb0a18af5e94bd032b3513a80aba65e7f0
Alexander Shcherbachenko <ascherbachenko@simtechdev.com>
2017-06-17 20:21:27 +0300

    opencv exmaple modified

--- opencv/capture.py
+++ opencv/capture.py
@@ -1,23 +1,16 @@
 import cv2
-
-# img = cv2.imread('galaxy.jpg')
-# cv2.imshow('Capturing', img)
-# key = cv2.waitKey(0)
-# cv2.destroyAllWindows()
+import matplotlib.pyplot as plt
 
 video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-    print(type(frame));
-    print(frame.shape);
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Capturing', gray)
+    plt.imshow(frame, cmap='gray')
+    plt.pause(0.01)
+    plt.draw()
+    key = plt.waitforbuttonpress(0.01)
 
-    key = cv2.waitKey(1000)
-
-    if key == ord('q'):
-        break
+    if key:
+        break;
 
 video.release()
-cv2.destroyAllWindows()
